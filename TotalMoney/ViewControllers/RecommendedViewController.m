e498db981a3e99b14e1baecde962764021469b19
xiaotang <soul_chaoyue@163.com>
2016-12-08 17:29:45 +0800

    ·

--- TotalMoney/ViewControllers/RecommendedViewController.m
+++ TotalMoney/ViewControllers/RecommendedViewController.m
@@ -8,7 +8,9 @@
 
 #import "RecommendedViewController.h"
 
-@interface RecommendedViewController ()
+@interface RecommendedViewController ()<UITableViewDelegate,UITableViewDataSource>
+
+@property (nonatomic,strong)UITableView *recordTabView;
 
 @end
 
@@ -18,21 +20,43 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     self.view.backgroundColor = [UIColor cyanColor];
+    [self setUI];
+}
+
+- (void)setUI{
+    [self.view addSubview:self.recordTabView];
+    [self.recordTabView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.left.right.equalTo(self.view);
+        make.height.equalTo(@300);
+    }];
+}
+
+#pragma mark --delegate
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"hehe" forIndexPath:indexPath];
+    return cell;
+}
+
+#pragma mark --lazyload
+- (UITableView *)recordTabView{
+    if (_recordTabView == nil) {
+        _recordTabView = [[UITableView alloc]init];
+        _recordTabView.delegate = self;
+        _recordTabView.dataSource = self;
+        _recordTabView.separatorStyle = UITableViewCellSeparatorStyleNone;
+        _recordTabView.backgroundColor = [UIColor whiteColor];
+        [_recordTabView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"hehe"];
+    }
+    return _recordTabView;
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
-
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
 
 @end
